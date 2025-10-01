57d9527ff7d1c19590782c7b899cc9636de53521
picmoB <ivan.munivrana99@gmail.com>
2025-10-01 15:44:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+/* FOR HAMBURGER MENU */
+
+let hamburgerLinks = document.querySelector(".hamburger-menu-links");
+
+function showAndHide(x) {
+    x.classList.toggle("change");
+    hamburgerLinks.classList.toggle("active");
+}
